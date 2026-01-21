58ed3efcf335c1a0d97d3c43633b8d2ad56760d1
balenav <victorbalenaes@gmail.com>
2026-01-21 20:33:22 -0300

    feat: adiciona template padrão das pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/**
+ * TESTING ROUTES
+ */
+    Route::view('/example-page','example-page');
+    Route::view('/example-auth','example-auth');
+
+/**
+ * ADMIN ROUTES
+ */
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::middleware([])->group(function(){
+        Route::controller(AuthController::class)->group(function(){
+            Route::get('/login','loginForm')->name('login');
+            Route::get('/forgot-password','forgotForm')->name('forgot');
+        });
+    });
+
+    Route::middleware([])->group(function () {
+        Route::controller(AdminController::class)->group(function(){
+            Route::get('/dashboard','adminDashboard')->name('dashboard');
+        });
+    });
 });

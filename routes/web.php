5de2e6cf3e7057abec9e975217215764fe5a9d21
balenav <victorbalenaes@gmail.com>
2026-03-21 20:41:04 -0300

    Feat: adiciona rota de categories

--- routes/web.php
+++ routes/web.php
@@ -39,15 +39,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::post('/update-profile-picture','updateProfilePicture')->name('update_profile_picture');
             Route::get('/settings','generalSettings')->name('settings');
             Route::post('/update-logo','updateLogo')->name('update_logo');
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             Route::post('/update-favicon','updateFavicon')->name('update_favicon');
-=======
             Route::get('/categories','categoriesPage')->name('categories');
->>>>>>> Stashed changes
-=======
-            Route::get('/categories','categoriesPage')->name('categories');
->>>>>>> Stashed changes
         });
     });
 });
